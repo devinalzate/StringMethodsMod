e2fe8f956c8edaaccd47d12b4ca6b8a7586e4e94
devinalzate <98599273+devinalzate@users.noreply.github.com>
2025-03-10 23:33:49 -0500

    Cambio de visibilidad

--- utils.go
+++ utils.go
@@ -1,23 +1,23 @@
 package utils
 
 type Product struct {
-	name     string
-	price    float64
-	overview string
+	Name     string
+	Price    float64
+	Overview string
 }
 
 func CreateProduct(name_new string, price_new float64, overview_new string) Product {
 	new_product := Product{}
-	new_product.name = name_new
-	new_product.price = price_new
-	new_product.overview = overview_new
+	new_product.Name = name_new
+	new_product.Price = price_new
+	new_product.Overview = overview_new
 	return new_product
 }
 
 func (p *Product) ChangeOverview(overview_update string) {
-	p.overview = overview_update
+	p.Overview = overview_update
 }
 
 func (p *Product) RecalculatePriceWithIva() {
-	p.price = p.price * 0.20
+	p.Price = p.Price * 0.20
 }
