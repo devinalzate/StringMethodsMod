21a4323f90be644d4935581dd051d6776f709c40
devinalzate <98599273+devinalzate@users.noreply.github.com>
2025-03-10 23:25:02 -0500

    Cambio modulo a producto

--- utils.go
+++ utils.go
@@ -1,7 +1,23 @@
 package utils
 
-import "fmt"
+type Product struct {
+	name     string
+	price    float64
+	overview string
+}
 
-func StringPRUEBA() {
-	fmt.Println("2")
+func CreateProduct(name_new string, price_new float64, overview_new string) Product {
+	new_product := Product{}
+	new_product.name = name_new
+	new_product.price = price_new
+	new_product.overview = overview_new
+	return new_product
+}
+
+func (p *Product) ChangeOverview(overview_update string) {
+	p.overview = overview_update
+}
+
+func (p *Product) RecalculatePriceWithIva() {
+	p.price = p.price * 0.20
 }
